ae744d1ae80ebd80a6af09793a7d708514d11714
theoopsguy <theoopsguy01@gmail.com>
2022-12-23 16:58:24 +0530

    Add more functions to contract and contract abi

--- contract/Eatbit.sol
+++ contract/Eatbit.sol
@@ -158,8 +158,175 @@ contract Eatbit {
             amount += tempItem.price;
         }
 
-        return (amount * 1 ether, true);
-        // return (amount, true);
+        // return (amount * 1 ether, true);
+        return (amount, true);
     }
 
-    
+    function placeOrder(
+        uint256[] memory itemIdArray,
+        string memory customerPhysicalAddress,
+        string memory customerName,
+        string memory customerContactNumber
+    ) public payable {
+        (uint256 amount, bool isSameRestraunt) = totalAmount(itemIdArray);
+        uint256 tempRestrauntId = itemList[itemIdArray[0]].restrauntId;
+
+        require(isSameRestraunt, "Do place the order from same restraunt");
+        require(
+            amount == msg.value,
+            "Please submit the asking price in order to complete the purchase"
+        );
+
+        wallet += msg.value;
+
+        orderList.push(
+            Order(
+                msg.sender,
+                itemIdArray,
+                amount,
+                tempRestrauntId,
+                false,
+                0,
+                customerPhysicalAddress,
+                customerName,
+                customerContactNumber,
+                block.timestamp
+            )
+        );
+        restrauntList[tempRestrauntId].orderId.push(orderNumber);
+
+        orderNumber++;
+    }
+
+    function checkOrdersRestraunt()
+        public
+        view
+        returns (uint256[] memory orderIds)
+    {
+        for (uint256 ind = 0; ind < restrauntList.length; ind++) {
+            if (msg.sender == restrauntList[ind].sender) {
+                return restrauntList[ind].orderId;
+            }
+        }
+
+        require(false, "Restraunt does not exist");
+
+        // uint256[] memory temp;
+        // return temp;
+    }
+
+    // only triverse upto tempnooforders , else are garbage
+    function checkOrdersCust()
+        public
+        view
+        returns (uint256[] memory array, uint256 size)
+    {
+        uint256[] memory ids = new uint256[](orderNumber);
+
+        uint256 tempNumberOfOrders = 0;
+        for (uint256 ind = 0; ind < orderList.length; ind++) {
+            if (msg.sender == orderList[ind].customerAdd) {
+                ids[tempNumberOfOrders] = ind;
+                tempNumberOfOrders++;
+            }
+        }
+
+        return (ids, tempNumberOfOrders);
+    }
+
+    function getRestrauntId() public view returns (uint256) {
+        for (uint256 ind = 0; ind < restrauntList.length; ind++) {
+            if (msg.sender == restrauntList[ind].sender) {
+                return ind;
+            }
+        }
+
+        require(false, "Restraunt does not exist");
+    }
+
+    function checkItemRestraunt(uint256 restrauntId)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        return restrauntList[restrauntId].itemId;
+    }
+
+    function getItemIdForOrder(uint256 orderId)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        return orderList[orderId].itemIds;
+    }
+
+    function acceptOrder(bool isAccept, uint256 orderId) public {
+        Order storage tempOrder = orderList[orderId];
+        require(
+            restrauntList[tempOrder.restrauntId].sender == msg.sender,
+            "Login from restraunt id"
+        );
+
+        if (isAccept) {
+            tempOrder.isAccepted = 1;
+        } else {
+            tempOrder.isAccepted = -1;
+            tempOrder.isFullfiled = true;
+
+            payable(tempOrder.customerAdd).transfer(tempOrder.amount);
+            wallet -= tempOrder.amount;
+        }
+    }
+
+    function orderComplete(uint256 orderId) public {
+        Order storage tempOrder = orderList[orderId];
+        require(
+            tempOrder.customerAdd == msg.sender,
+            "You are not the one who ordered"
+        );
+        require(tempOrder.isAccepted != 0, "Order is not yet even acceted");
+        require(
+            tempOrder.isAccepted != -1,
+            "Order was rejected by the restraunt.Your money would have been returned"
+        );
+        require(tempOrder.isFullfiled == false, "Already completed");
+        tempOrder.isFullfiled = true;
+
+        payable(restrauntList[tempOrder.restrauntId].sender).transfer(
+            tempOrder.amount
+        );
+        wallet -= tempOrder.amount;
+    }
+
+    function orderDetails(uint256 orderId) public view returns (Order memory) {
+        Order memory tempOrder = orderList[orderId];
+        require(
+            tempOrder.customerAdd == msg.sender ||
+                restrauntList[tempOrder.restrauntId].sender == msg.sender,
+            "Unauthorized"
+        );
+
+        return tempOrder;
+    }
+
+    uint256 private interval = 1 days;
+
+    function chainLinkAutomation() public onlyOwner {
+        for (uint256 ind = 0; ind < orderNumber; ind++) {
+            if (
+                orderList[ind].isFullfiled == false &&
+                block.timestamp - orderList[ind].timeStamp > interval
+            ) {
+                payable(orderList[ind].customerAdd).transfer(
+                    orderList[ind].amount
+                );
+                wallet -= orderList[ind].amount;
+                orderList[ind].isFullfiled = true;
+            }
+        }
+    }
+
+    function changeInterval(uint256 newInterval) public onlyOwner {
+        interval = newInterval;
+    }
+}
