034d91d9874fd0624d7dff58a29e95dcfde8260a
tanishchugh01 <tanishchugh01@gmail.com>
2023-01-03 23:19:34 +0530

    Customer order table working

--- eatBit-ui/src/components/Customer-order/index.js
+++ eatBit-ui/src/components/Customer-order/index.js
@@ -16,7 +16,7 @@ import Link from "next/link";
 import { Ban, Dots, Select } from "tabler-icons-react";
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
-import { checkOrdersCustomer, orderDetails } from "api";
+import { checkOrdersCustomer, itemList, orderDetails } from "api";
 // Components
 const Navbar = dynamic(() => import("components/Navbar"), { ssr: false });
 const MarkOrderComplete = dynamic(
@@ -81,21 +81,37 @@ export default function CustomerOrder() {
     });
   };
 
-  const parseOrder = (str, orderId) => {
+  const parseOrder = async (str, orderId) => {
     const ele = str.split(",");
-    // console.log(ele);
+
+    const itemArraySize = Number(ele[ele.length - 1] - 1);
+    let itemArray = [];
+
+    for (let ind = 1; ind < itemArraySize + 2; ind++) {
+      itemArray.push(ele[ind]);
+    }
+
+    console.log(itemArray);
+
+    const itemPromise = (itemArray = itemArray.map(async (item) => {
+      return itemList(item).then((res) => {
+        // console.log(res)
+        return res[0];
+      });
+    }));
+
     const order = {
       _id: orderId,
       custAddress: ele[0],
-      itemIds: ele[1],
-      amount: ele[2],
-      restrauntId: ele[3],
-      isFullfiled: ele[4],
-      isAccepted: ele[5],
-      custPhysicalAddress: ele[6],
-      custName: ele[7],
-      custcontactNumber: ele[8],
-      timeStamp: ele[9],
+      itemArray: await Promise.all(itemPromise),
+      amount: ele[2 + itemArraySize],
+      restrauntId: ele[3 + itemArraySize],
+      isFullfiled: ele[4 + itemArraySize],
+      isAccepted: ele[5 + itemArraySize],
+      custPhysicalAddress: ele[6 + itemArraySize],
+      custName: ele[7 + itemArraySize],
+      custcontactNumber: ele[8 + itemArraySize],
+      timeStamp: ele[9 + itemArraySize],
     };
     return order;
   };
@@ -103,7 +119,7 @@ export default function CustomerOrder() {
   const fetchOrders = async (orderIdArray) => {
     let orderList = [];
 
-    const orderPromises = orderIdArray.map((item) => {
+    const orderPromises = orderIdArray.map(async (item) => {
       return orderDetails(item).then((res) => {
         return parseOrder(res, item);
       });
@@ -134,11 +150,18 @@ export default function CustomerOrder() {
     <div style={{ height: "100%", width: "100%" }}>
       <Navbar />
       <div className="w-full py-5 px-5 flex justify-between bg-white">
-        <h3
-          onClick={LoadOrders}
-          class="font-medium leading-tight text-3xl mt-0 mb-2 text-black-600">
+        <h3 class="font-medium leading-tight text-3xl mt-0 mb-2 text-black-600">
           Order List
         </h3>
+        {/* add reload button */}
+        <div className="flex flex-row justify-end">
+          <button
+            onClick={LoadOrders}
+            className="flex flex-row justify-center items-center bg-white border-2 border-black-600 rounded-md px-3 py-2 text-black-600 hover:bg-black-600 hover:text-white">
+            <Dots size={20} strokeWidth={1.5} />
+            <span className="ml-2">Reload</span>
+          </button>
+        </div>
       </div>
       <div className="flex flex-col justify-start w-full h-full py-3 px-5 bg-white">
         <Table
@@ -171,6 +194,7 @@ export default function CustomerOrder() {
               return (
                 <tr
                   key={item._id}
+                  itemId={item._id}
                   className={cx({ [classes.rowSelected]: selected })}>
                   {/* <td>
                     <Checkbox
@@ -180,7 +204,14 @@ export default function CustomerOrder() {
                     />
                   </td> */}
                   <td>{item.custPhysicalAddress}</td>
-                  <td>{`${item.itemIds[0]}`}</td>
+                  <td>
+                    {item.itemArray.map((food, ind) => {
+                      let str = food;
+                      if (ind !== item.itemArray.length - 1) str += ", ";
+
+                      return str;
+                    })}
+                  </td>
                   <td>{item.amount}</td>
 
                   <td className="flex items-center">
