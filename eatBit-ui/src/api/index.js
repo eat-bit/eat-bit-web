cc3bb6560ba68734fadadd5c22ff6fba833b6595
tanishchugh01 <tanishchugh01@gmail.com>
2023-01-06 03:50:33 +0530

    restraunt menu now corrected

--- eatBit-ui/src/api/index.js
+++ eatBit-ui/src/api/index.js
@@ -135,13 +135,14 @@ export async function placeOrder(itemArr, address, fullname, customerContact, pr
 export async function getRestrauntId() {
     const contract = new ethers.Contract(contractAddress, contractAbi, provider);
     const txResponse = await contract.connect(signer).getRestrauntId();
-    await console.log(txResponse.toString());
+    // await console.log(txResponse.toString());
+    return txResponse;
 }
 
 export async function checkItemRestraunt(idx) {
     const contract = new ethers.Contract(contractAddress, contractAbi, provider);
     const txResponse = await contract.connect(signer).checkItemRestraunt(idx);
-    return txResponse.toString();
+    return txResponse;
 }
 
 export async function getItemIdForOrder() {
