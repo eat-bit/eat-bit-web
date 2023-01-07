9c7573fbbdfacd9e41eca1e9b77b7f187bdecebf
Tanish Chugh <72879445+tanishchugh01@users.noreply.github.com>
2023-01-08 01:50:38 +0530

    parse error function corrected (#47)

--- eatBit-ui/src/global/parseErrorMessage.js
+++ eatBit-ui/src/global/parseErrorMessage.js
@@ -1,21 +1,20 @@
 export default function parseErrorMessage(err) {
   err = err.message;
 
-  if (err.includes("User denied transaction signature")) {
+  try {
     const errorMessage = err.split("reason=")[1].split('"')[1];
 
     return errorMessage;
-  }
+  } catch (err) {
+    // if (err.includes("insufficient funds")) {
+    //   return "Insufficient funds";
+    // }
 
-  if (err.includes("insufficient funds")) {
-    return "Insufficient funds";
-  }
-  
-  if (err.includes("execution reverted")) {
-    const errorMessage = err.split("execution reverted:")[1].split('"')[1];
+    // if (err.includes("execution reverted")) {
+    //   const errorMessage = err.split("execution reverted:")[1].split('"')[1];
 
-    return errorMessage;
+    //   return errorMessage;
+    // }
+    return "An error encounterd. Please try again";
   }
-  
-  return "An error encounterd. Please try again"
 }
